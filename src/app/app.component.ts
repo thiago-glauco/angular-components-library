4e1698febff503939655802bc410cc92d863b328
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-10-23 10:27:24 -0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FacebookService, LoginResponse, LoginOptions, UIResponse, UIParams, FBVideoComponent } from 'ngx-facebook';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  constructor(
+    private fb: FacebookService
+  ) {
+
+    console.log('Initializing Facebook');
+
+    fb.init({
+      appId: '1927971220769787',
+      version: 'v4.0'
+    });
+
+}
 }
